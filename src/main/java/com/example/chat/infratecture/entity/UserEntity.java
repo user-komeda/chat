99011de9c4b269e3150d4c50224633387d413a61
user_komeda <shigoto922@gmail.com>
2022-02-25 00:04:30 +0900

    #1 ユーザ登録機能を作る
    ユーザを登録できるようにする

--- src/main/java/com/example/chat/infratecture/entity/UserEntity.java
+++ src/main/java/com/example/chat/infratecture/entity/UserEntity.java
@@ -2,6 +2,7 @@ package com.example.chat.infratecture.entity;
 
 import com.example.chat.domain.object.User;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
@@ -10,14 +11,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Table("user")
+@Table("chat_user")
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
+@Id
+private  int Id;
 
   private String email;
 
   private String password;
+
+  public UserEntity(String email, String password) {
+    this.email=email;
+    this.password=password;
+  }
 
   public User toDomainUser() {
     return new User(this.email, this.password);
